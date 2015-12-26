3f31de9c5d4895684d66a593f7faf98479303b0c
Dan Wasyluk <dwasyluk@gmail.com>
2015-12-26 02:29:02 -0500

    revert accidental push

---
+++ index.js
@@ -0,0 +1,9 @@
+var pricepeg = require('./lib/PricePeg');
+
+
+console.log("runin");
+var peg1 = pricepeg.PricePeg("peg1");
+var peg2 = pricepeg.PricePeg("peg2");
+
+peg1.testOutput();
+peg2.testOutput();

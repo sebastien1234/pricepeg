02892ddaad51ac0cf7150042216777ee0cc40cbd
Dan Wasyluk <dwasyluk@gmail.com>
2015-12-26 03:57:05 -0500

    Changed away from revealing module (closure type) pattern for cleaner inheritance. Working on sorting out preffering inheritance patterns for backwards compatability with browser (not making it node-only inheritance, etc)- prototypical seems best.

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-var pricepeg = require('./lib/PricePeg');
+var PricePeg = require('./lib/PricePeg').PricePeg;
+//var TestDataSource = require('./lib/data/TestDataSource').TestDataSource;
+var BaseConversionDataSource = require('./lib/data/BaseConversionDataSource').BaseConversionDataSource;
 
 
 console.log("runin");
-var peg1 = pricepeg.PricePeg("peg1");
-var peg2 = pricepeg.PricePeg("peg2");
+var peg1 = new PricePeg("peg1");
+var peg2 = new PricePeg("peg2");
 
 peg1.testOutput();
 peg2.testOutput();
+
+peg1.pegStr = "changed";
+
+peg1.testOutput();
+
+//test inheritance assumptions
+var baseData = new BaseConversionDataSource("test base");
+//var extendData = new TestDataSource("test extend");
+
+//console.log("extended:" + (extendData instanceof TestDataSource))
+

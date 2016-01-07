120bf0ad3da2f5d52d00bba893d13d5e8f97b6da
Dan Wasyluk <dwasyluk@gmail.com>
2016-01-06 23:55:27 -0500

    Work in progress fetching and averaging SYS/BTC price. Issues with current scoping and modules need to be wrapped. Other TODOs in code.

--- index.js
+++ index.js
@@ -6,14 +6,7 @@ var PricePeg = require('./lib/PricePeg').PricePeg;
 
 console.log("runin");
 
-//testing responses
-var bittrexSYSData = new BittrexDataSource("https://bittrex.com/api/v1.1/public/getticker?market=BTC-SYS");
-var poloniexSYSData = new PoloniexDataSource("https://poloniex.com/public?command=returnTicker");
-var cryptsySYSData = new CryptsyDataSource("http://pubapi.cryptsy.com/api.php?method=singlemarketdata&marketid=278");
-var coinbaseBTCData = new CoinbaseDataSource("https://coinbase.com/api/v1/currencies/exchange_rates");
+var peg = new PricePeg();
 
-
-bittrexSYSData.fetchCurrencyConversionData();
-poloniexSYSData.fetchCurrencyConversionData();
-cryptsySYSData.fetchCurrencyConversionData();
-coinbaseBTCData.fetchCurrencyConversionData();
+//TODO: this is so hacky, replace with proper promises
+setTimeout(function() { peg.getSYSBTCAverage() }, 2000);
